bb261bfc048d9167fb6a2a938513bb2984df9ef7
adelic-penguins <esperanto0616@gmail.com>
2025-12-13 20:39:22 +0900

    モノレポ構成用biome設定を修正

--- front/src/routes/AppRouter.tsx
+++ front/src/routes/AppRouter.tsx
@@ -1,11 +1,11 @@
 import { Navigate, Route, Routes } from "react-router";
 
 const AppRouter = () => {
-    return (
-        <Routes>
-            <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-    );
+	return (
+		<Routes>
+			<Route path="*" element={<Navigate to="/" replace />} />
+		</Routes>
+	);
 };
 
 export default AppRouter;
